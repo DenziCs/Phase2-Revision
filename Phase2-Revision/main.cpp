c6f825b60b5a93f614ebb57bdb21edea7db380c9
DenziCs <78402731+DenziCs@users.noreply.github.com>
2021-05-11 13:43:27 +0800

    Add files via upload

---
+++ Phase2-Revision/main.cpp
@@ -0,0 +1,17 @@
+/*
+Jose Miguel D. Bravo
+Nathaniel Francis S. Filoteo
+GD-PHYS S11
+MP Phase 02
+*/
+
+#include"Custom Classes/Game.h"
+using namespace std;
+using namespace gdphys;
+
+int main() {
+	Game game;
+	game.run();
+
+	return EXIT_SUCCESS;
+}
